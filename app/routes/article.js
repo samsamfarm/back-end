ce4bc96fd392fe731ab2c591b88de18e71104372
ParkYoungJun123 <aptl741@gmail.com>
2023-05-03 00:36:53 +0900

    Feat: article post

--- app/routes/article.js
+++ app/routes/article.js
@@ -10,6 +10,24 @@
 const connection = require("../app.js");
 const router = express.Router();
 
-router.post("/", (rep, res) => {});
+router.post("/", (rep, res) => {
+  const { title, content } = req.body;
+  connection.query(
+    "INSERT INTO Posts (title,content)VALUES (??)",
+    [title, content],
+    (error, result) => {
+      if (error) {
+        console.error("insert error", error);
+        res.status(500).send("error");
+      } else {
+        res.status(200).send("create article success");
+      }
+    }
+  );
+});
+
+router.get("/", (req, res) => {
+  connection.query("");
+});
 
 exports.router = articleRouter;
