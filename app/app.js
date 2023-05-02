95f7c05f34baf88ab0e4f75f9e044fa868819acf
ParkYoungJun123 <aptl741@gmail.com>
2023-05-03 00:34:54 +0900

    Feat: article_post 추가

--- app/app.js
+++ app/app.js
@@ -8,29 +8,11 @@ const port = process.env.APP_PORT;
 const app = express();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connectiong to database", err);
-  } else {
-    console.log("Connectd to Database!");
-  }
-});
-
-const mysql = require("mysql2");
-
-const connection = mysql.createConnection({
-  host: "34.64.51.215",
-  port: 5001,
-  database: "samsamfarm",
-  user: "root",
-  password: "ghskfen@@",
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DB,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PW,
 });
 
 connection.connect((err) => {
@@ -45,11 +27,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-<<<<<<< Updated upstream
-app.get("/books", (req, res) => {
-=======
 app.get("/TEST", (req, res) => {
->>>>>>> Stashed changes
   connection.query("SELECT * FROM Users", (error, result) => {
     if (error) {
       console.error("디비 쿼리 에러", error);
@@ -60,6 +38,10 @@ app.get("/TEST", (req, res) => {
     }
   });
 });
+
+const articleRouter = require("./routes/article");
+
+app.get("article", articleRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- app/routes/article.js
+++ app/routes/article.js
@@ -6,3 +6,10 @@
 // 댓글 추가하기
 // 댓글 삭제하기
 // 댓글 수정하기
+
+const connection = require("../app.js");
+const router = express.Router();
+
+router.post("/", (rep, res) => {});
+
+exports.router = articleRouter;
