add89a3d196de9a9d27ed5ef8a711dce47f4957a
smith <aptl741@gmail.com>
2023-05-13 04:33:01 +0900

    feat: 디바이스 마이그레이션 업데이트

--- migrations/20230510162414_devices.js
+++ migrations/20230510162414_devices.js
@@ -38,7 +38,11 @@ exports.up = function(knex) {
         table.integer("id").primary();
         table.integer("user_id").unsigned();
         table.timestamp("created_at").defaultTo(knex.fn.now());
-        table.datetime("updated_at");
+        table
+          .datetime("updated_at")
+          .defaultTo(
+            Database.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+          );
         table.datetime("deleted_at");
         table.foreign("user_id").references("users.id");
     });
